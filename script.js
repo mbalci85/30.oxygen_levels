8ea373b88e7456f98d53f69b3168795ea8bc9e30
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-01-11 23:42:13 +0000

    added function

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 Many years into the future, a team of Space Voyagers find their ship is low on Oxygen and need to dock
 somewhere safe while they call home for help.
   
-Their computer detects a list of nearby planets that have Oxygen in their atmosphere. It has produced an array of their Oxygen levels.
+Their computer detects a list of nearby planets that have Oxygen in their atmosphere. It has produced an array of 
+their Oxygen levels.
 
 To be safe to land on, a planet needs to have an Oxygen level between 19.5% and 23.5%.
 
@@ -18,3 +19,17 @@ const oxygenLevelsVirgo = [
 	'20.2%',
 ];
 const oxygenLevelsUrsa = ['30.8%', '22.5%', '18.8%', '19.5%', '20.2%', '31.6%'];
+
+const oxygenSafeLevels = (arr) => {
+	let res = [];
+	arr.forEach((level) => {
+		if (parseFloat(level) < 23.5 && parseFloat(level) > 19.5) {
+			return res.push(level);
+		}
+	});
+	return `The first safe level of oxygen is ${
+		res[0]
+	} all safe levels are ${res.join(', ')}`;
+};
+
+console.log(oxygenSafeLevels(oxygenLevelsVirgo));
