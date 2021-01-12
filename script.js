acc0676cb2f5491f7b45262567956e941fbfbfa1
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-01-12 23:35:46 +0000

    added array.every method

--- script.js
+++ script.js
@@ -33,3 +33,27 @@ const oxygenSafeLevels = (arr) => {
 };
 
 console.log(oxygenSafeLevels(oxygenLevelsVirgo));
+
+/*
+  The space travellers have safely landed and are foraging for food in the natural wildlife.
+
+  There are bushes with many different colour berries.
+  The pink berries are the ONLY safe ones to eat.
+  If any other berries are present, it's best not to eat from the bush at all!
+
+  Create a function which checks if the bush has ALL PINK berries and is safe for the astronauts to eat from the bush.
+
+*/
+
+let bushBerryColours1 = ['pink', 'pink', 'pink', 'neon', 'pink', 'transparent'];
+let bushBerryColours2 = ['pink', 'pink', 'pink', 'pink'];
+
+const checkBushes = (arr) => {
+	if (arr.every((bush) => bush == 'pink')) {
+		return `This planet is safe`;
+	} else {
+		return `This planet is not safe`;
+	}
+};
+
+console.log(checkBushes(bushBerryColours2));
